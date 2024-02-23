664e4caeebc26f348b582b0a8712f851e4df86c2
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-24 00:19:19 +0200

    refactor: update welcome-page ui and validate inputs

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,70 @@
-import Link from "next/link";
-import { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "Welcome page",
-};
+import { ChangeEvent, FormEvent, useState } from "react";
+import getRandomEmoji from "./util/random-emoji";
 
 export default function Home() {
+  const [currentIcon, setCurrentIcon] = useState("😄");
+  const [username, setUsername] = useState("");
+  const [formError, setFormError] = useState<string>("");
+
+  function handleInputChange({ target }: ChangeEvent<HTMLInputElement>) {
+    setFormError("");
+    setUsername(target.value);
+  }
+
+  function handleIconChange() {
+    setFormError("");
+    setCurrentIcon(getRandomEmoji());
+  }
+
+  function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (!username) return setFormError("Enter username please!");
+    console.log("username is fine");
+  }
+
   return (
-    <main className="h-dvh grid place-content-center">
-      <div className="flex flex-col items-center gap-5">
-        <p className="sm:text-xl md:text-2xl lg:text-4xl">
-          Welcome to the chat app
-        </p>
-        <div className="flex sm:gap-5 flex-col sm:flex-row w-full sm:w-auto gap-2 *:text-center *:bg-blue-600 *:px-6 *:py-1 *:rounded-full *:transition">
-          <Link href="/auth/login" className="hover:bg-blue-400">
-            Login
-          </Link>
-          <Link href="/auth/register" className="hover:bg-blue-400">
-            Register
-          </Link>
-        </div>
-      </div>
-    </main>
+    <>
+      <title>Welcome page</title>
+      <main className="min-h-dvh grid items-center px-5 sm:justify-center">
+        <form
+          onSubmit={handleFormSubmit}
+          className="flex flex-col items-center gap-5"
+        >
+          <p className="sm:text-xl md:text-2xl lg:text-4xl">
+            Welcome to the chat app
+          </p>
+          <label htmlFor="username" className="w-full flex flex-col">
+            <input
+              type="text"
+              className="text-white text-center text-xs sm:text-sm sm:w-auto bg-neutral-700 rounded-full px-4 py-2 focus:outline-none"
+              value={username}
+              onChange={handleInputChange}
+              name="username"
+              placeholder="Username"
+            />
+            {formError && (
+              <p className="text-sm text-red-600 text-center">{formError}</p>
+            )}
+          </label>
+          <div className="flex items-center gap-5">
+            <span className="text-3xl p-1 rounded-md bg-neutral-700 grid place-content-center">
+              {currentIcon}
+            </span>
+            <button
+              type="button"
+              onClick={handleIconChange}
+              className="rounded-full bg-blue-600/30 transition hover:bg-blue-600/70 px-4 py-1 text-sm"
+            >
+              Change Icon
+            </button>
+          </div>
+          <button className="hover:bg-blue-800 bg-blue-600 w-full mt-5 px-10 rounded-full py-1">
+            Join
+          </button>
+        </form>
+      </main>
+    </>
   );
 }
