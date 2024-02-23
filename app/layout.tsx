4dd6f62ec2be944e6b751e4bf48d9636309739ff
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-24 00:18:44 +0200

    feat: initialize react-query in whole app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import classNames from "classnames";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,16 +12,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const client = new QueryClient();
+
   return (
     <html lang="en">
-      <body
-        className={classNames(
-          inter.className,
-          "bg-neutral-900 text-white max-w-screen-2xl mx-auto min-h-dvh",
-        )}
-      >
-        {children}
-      </body>
+      <QueryClientProvider client={client}>
+        <body
+          className={classNames(
+            inter.className,
+            "bg-neutral-900 text-white max-w-screen-2xl mx-auto min-h-dvh",
+          )}
+        >
+          {children}
+        </body>
+      </QueryClientProvider>
     </html>
   );
 }
