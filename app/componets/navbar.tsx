2dbc5b47bdb5780ce47805455effc826d9c37ed6
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-24 13:00:46 +0200

    feat: add active class on navigation

--- app/componets/navbar.tsx
+++ app/componets/navbar.tsx
@@ -5,16 +5,35 @@ import { BiMessageRoundedDetail } from "react-icons/bi";
 import { IoPeopleOutline } from "react-icons/io5";
 import { useAppSelector } from "../store/hooks";
 import UserIcon from "./user-icon";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
   const userEmoji = useAppSelector((state) => state.user.userData?.emoji);
+  const pathname = usePathname();
 
   return (
     <div className="bg-neutral-700 relative flex gap-5 items-center text-xl justify-center py-2">
-      <Link href="/dms" className="p-2 bg-blue-600 text-white rounded-full">
+      <Link
+        href="/dms"
+        className={classNames(
+          "p-2 transition hover:bg-blue-500 text-white rounded-full",
+          {
+            "bg-blue-600": pathname === "/dms",
+          },
+        )}
+      >
         <BiMessageRoundedDetail />
       </Link>
-      <Link href="/groups">
+      <Link
+        href="/groups"
+        className={classNames(
+          "p-2 transition hover:bg-blue-500 text-white rounded-full",
+          {
+            "bg-blue-600": pathname === "/groups",
+          },
+        )}
+      >
         <IoPeopleOutline />
       </Link>
       <UserIcon
