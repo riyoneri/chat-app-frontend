77289754dcebf309d599d4d3fe596bc0e46cb964
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-25 09:53:07 +0200

    feat: create groups page

---
+++ app/(home)/groups/page.tsx
@@ -0,0 +1,16 @@
+import ChatList from "@/app/componets/chats/chats-list";
+import { Chats } from "../layout";
+
+export default function DirectMessages() {
+  return (
+    <>
+      <title>Group messages</title>
+      <section className="flex-1 flex">
+        <ChatList chats={Chats} />
+        <section className="grid flex-1 place-content-center">
+          No Chat opened.
+        </section>
+      </section>
+    </>
+  );
+}
