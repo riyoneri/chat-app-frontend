8f32eb5fcb09b88e871080e61783cd540643818c
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-03 21:17:13 +0200

    refactor: update navbar ui

--- app/componets/navbar.tsx
+++ app/componets/navbar.tsx
@@ -1,45 +1,19 @@
 "use client";
 
 import Link from "next/link";
-import { BiMessageRoundedDetail } from "react-icons/bi";
-import { IoPeopleOutline } from "react-icons/io5";
-import { useAppSelector } from "../store/hooks";
-import UserIcon from "./user-icon";
-import classNames from "classnames";
-import { usePathname } from "next/navigation";
 
 export default function NavBar() {
-  const userEmoji = useAppSelector((state) => state.user.userData?.emoji);
-  const pathname = usePathname();
-
   return (
-    <div className="bg-neutral-700 relative flex gap-5 items-center text-xl justify-center py-2">
-      <Link
-        href="/dms"
-        className={classNames(
-          "p-2 transition hover:bg-blue-500 text-white rounded-full",
-          {
-            "bg-blue-600": pathname === "/dms",
-          },
-        )}
-      >
-        <BiMessageRoundedDetail />
+    <div className="bg-neutral-800 *:transition relative text-xs sm:text-sm flex gap-5 items-center justify-center py-2 sm:py-3">
+      <Link href="/chats" className="bg-sky-800 px-3 rounded-full">
+        Chats
       </Link>
-      <Link
-        href="/groups"
-        className={classNames(
-          "p-2 transition hover:bg-blue-500 text-white rounded-full",
-          {
-            "bg-blue-600": pathname === "/groups",
-          },
-        )}
-      >
-        <IoPeopleOutline />
+      <Link href="/chats" className="hover:bg-sky-800 px-3 rounded-full">
+        Groups
       </Link>
-      <UserIcon
-        icon={userEmoji ?? ""}
-        className="text-base sm:absolute right-5"
-      />
+      <Link href="/chats" className="hover:bg-sky-800 px-3 rounded-full">
+        People
+      </Link>
     </div>
   );
 }
