62b498e2e3c457807c549154c1e2afd2a11fb5f6
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-03 21:18:50 +0200

    feat: create dummy messages

--- app/componets/messages/messages-list.tsx
+++ app/componets/messages/messages-list.tsx
@@ -1,25 +1,49 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import MessagesListItem from "./messages-list-item";
+dayjs.extend(relativeTime);
 
 export interface Message {
   _id: string;
   chatId: string;
-  sender: string;
+  senderId: string;
   text: string;
   createdAt: string;
 }
 
-interface MessageListProperties {
-  messages: Message[];
-}
+const Messages: Message[] = [
+  {
+    _id: "1",
+    chatId: "1",
+    senderId: "2",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat a,
+            optio beatae temporibus debitis earum soluta quo, totam ab sit
+            perferendis aliquid? Minus veritatis, tempore quam magnam sit
+            delectus quasi.`,
+    createdAt: new Date(
+      new Date().setDate(new Date().getDate() - 1),
+    ).toISOString(),
+  },
+  {
+    _id: "2",
+    chatId: "1",
+    senderId: "1",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat a,
+            optio beatae temporibus debitis earum soluta quo, totam ab sit
+            perferendis aliquid? Minus veritatis, tempore quam magnam sit
+            delectus quasi.`,
+    createdAt: new Date(
+      new Date().setMinutes(new Date().getMinutes() - 20),
+    ).toISOString(),
+  },
+];
 
-export default function MessagesList({ messages }: MessageListProperties) {
+export default function MessagesList() {
   return (
-    <div className="flex-1 py-1 bg-black">
-      <div className="overflow-y-auto h-[calc(100dvh-9rem)] sm:h-[calc(100dvh-10rem)] scrollbar-none">
-        {messages.map((message) => (
-          <MessagesListItem {...message} key={message._id} />
-        ))}
-      </div>
+    <div className="overflow-y-auto flex flex-col h-[calc(100dvh-9.8rem)] sm:h-[calc(100dvh-10.5rem)] gap-2 scrollbar-none p-2 text-xs sm:text-sm">
+      {Messages.map((message) => (
+        <MessagesListItem {...message} key={message._id} displayDate />
+      ))}
     </div>
   );
 }
