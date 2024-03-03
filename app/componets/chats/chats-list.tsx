b3ca4dc9f24b9c684c8212e196bdeae900a22f19
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-03 21:44:24 +0200

    feat: create dummy chats

--- app/componets/chats/chats-list.tsx
+++ app/componets/chats/chats-list.tsx
@@ -1,26 +1,62 @@
-"use client";
-
-import { Chat } from "../../(home)/layout";
-import { useAppSelector } from "../../store/hooks";
 import ChatListItem from "./chats-list-item";
+const date = new Date();
+date.setDate(date.getDate() - 1);
 
-interface MainUiProperties {
-  chats: Chat[];
-  messages?: [];
+export interface Chat {
+  _id: string;
+  participant: {
+    name: string;
+    imageUrl: string;
+  };
+  lastMessage: {
+    ownerId: string;
+    text: string;
+    createdAt: string;
+  };
+  unreadMessages: number;
 }
 
-export default function MainUi({ chats }: MainUiProperties) {
-  const username = useAppSelector((state) => state.user.userData?.username);
+const Chats: Chat[] = [
+  {
+    _id: "1",
+    participant: {
+      name: "Lara Muler",
+      imageUrl: "https://docs.material-tailwind.com/img/face-2.jpg",
+    },
+    lastMessage: {
+      ownerId: "1",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          corrupti sit. Quo fugit non voluptate in a voluptatum voluptatibus
+          inventore laborum maiores! Temporibus veritatis voluptates est
+          voluptatum. Suscipit, aliquid ipsum.`,
+      createdAt: date.toISOString(),
+    },
+    unreadMessages: 2,
+  },
+  {
+    _id: "2",
+    participant: {
+      name: "Lionel Kaneza",
+      imageUrl: "https://docs.material-tailwind.com/img/face-2.jpg",
+    },
+    lastMessage: {
+      ownerId: "2",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          corrupti sit. Quo fugit non voluptate in a voluptatum voluptatibus
+          inventore laborum maiores! Temporibus veritatis voluptates est
+          voluptatum. Suscipit, aliquid ipsum.`,
+      createdAt: date.toISOString(),
+    },
+    unreadMessages: 20,
+  },
+];
+
+export default function ChatList() {
   return (
-    <section className="w-1/4 min-w-32 border-r text-xs sm:text-sm overflow-y-auto scrollbar-thin h-[calc(100dvh-3.4rem)] divide-y">
-      <p className="sticky top-0 p-2 text-right font-bold bg-neutral-900">
-        {username}
-      </p>
-      <div className="divide-y">
-        {chats.length > 0
-          ? chats.map((chat) => <ChatListItem {...chat} key={chat._id} />)
-          : "lion"}
-      </div>
+    <section className="text-sm *:px-2 scrollbar-none *:py-3 sm:h-[calc(100dvh-8.4rem)] overflow-y-auto space-y-1">
+      {Chats.map((chat) => (
+        <ChatListItem {...chat} key={chat._id} />
+      ))}
     </section>
   );
 }
