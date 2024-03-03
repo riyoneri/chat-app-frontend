ca2145d6fa3f755ee5d268e6806306f99af1a9a4
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-03 14:32:11 +0200

    refactor: add material-tailwind components in tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
