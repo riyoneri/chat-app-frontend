f8ded89ea6f89465be98c34cfbad8f308082aafb
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-03 23:28:29 +0200

    refactor: reduce navigation padding

--- app/componets/navbar.tsx
+++ app/componets/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div className="bg-neutral-800 *:transition relative text-xs sm:text-sm flex gap-5 items-center justify-center py-2 sm:py-3">
+    <div className="bg-neutral-800 *:transition relative text-xs sm:text-sm flex gap-5 items-center justify-center py-2">
       <Link href="/chats" className="bg-sky-800 px-3 rounded-full">
         Chats
       </Link>
