915db332da894e55c34bb056c4ac7489a3fdfec0
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-04 15:27:59 +0200

    feat: create new-chat-list-item component

---
+++ app/componets/new-chat-list-item.tsx
@@ -0,0 +1,19 @@
+import { Avatar } from "@material-tailwind/react";
+import { NewChat } from "../(home)/chats/new/page";
+
+export default function NewChatListItem({ imageUrl, name, username }: NewChat) {
+  return (
+    <div className="flex items-center justify-between hover:bg-ui-dark px-2 py-1 rounded-sm">
+      <div className="flex gap-4 items-center">
+        <Avatar src={imageUrl} alt="Avatar" placeholder={undefined} size="sm" />
+        <div>
+          <p className="font-bold">{name}</p>
+          <p className="text-neutral-400">{username}</p>
+        </div>
+      </div>
+      <button className="bg-sky-800 px-4 py-1 rounded-sm hover:bg-sky-600 transition">
+        Start Chat
+      </button>
+    </div>
+  );
+}
