6eaf4b559fe3fb6ed3a71bed9b0095c1554e08ae
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-06 21:15:57 +0200

    feat: create getAllUsers fetcher function

--- app/util/fetchers.ts
+++ app/util/fetchers.ts
@@ -2,6 +2,9 @@ import { LoginFormData } from "../auth/login/page";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+const getLocalStorageToken = () =>
+  localStorage.getItem("_n")?.replaceAll('"', "");
+
 export const createUser = async (body: FormData) => {
   const response = await fetch(`${API_URL}/auth/register`, {
     method: "POST",
@@ -27,6 +30,24 @@ export const authenticateUser = async (body: LoginFormData) => {
   });
 
   const data = await response.json();
+
+  if (!response.ok) {
+    throw data;
+  }
+
+  return data;
+};
+
+export const getAllUsers = async (page?: number) => {
+  const response = await fetch(`${API_URL}/users?page=${page}`, {
+    headers: {
+      Authorization: `Bearer ${getLocalStorageToken()}`,
+    },
+  });
+
+  const data = await response.json();
+
+  if (response.status === 401) throw new Error("401");
 
   if (!response.ok) {
     throw data;
