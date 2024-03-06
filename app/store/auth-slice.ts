454e773105a048c598392d4e849cfdd24b6e2206
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-06 09:36:34 +0200

    refactor: add isAuthenticated in property in auth-slice

--- app/store/auth-slice.ts
+++ app/store/auth-slice.ts
@@ -4,20 +4,23 @@ import { UserDto } from "../util/api";
 interface AuthState {
   user?: UserDto;
   token?: string;
+  isAuthenticated: boolean;
 }
 
-const initialState: AuthState = {};
+const initialState: AuthState = { isAuthenticated: false };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login(state, action: PayloadAction<AuthState>) {
-      state = action.payload;
+    login(state, action: PayloadAction<Omit<AuthState, "isAuthenticated">>) {
+      state = { ...action.payload, isAuthenticated: true };
+      return state;
     },
     logout(state) {
       delete state.token;
       delete state.user;
+      state.isAuthenticated = false;
     },
   },
 });
