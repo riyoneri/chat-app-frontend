0a4f253461c4f12aed7af41f8b4d7adac44c5921
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-06 09:47:56 +0200

    feat: implement logout

--- app/componets/navbar.tsx
+++ app/componets/navbar.tsx
@@ -1,19 +1,57 @@
 "use client";
 
 import Link from "next/link";
+import { useSnackbar } from "notistack";
+import { IoMdLogOut } from "react-icons/io";
+import { useLocalStorage } from "usehooks-ts";
+import { authActions } from "../store/auth-slice";
+import { useAppDispatch } from "../store/hooks";
 
 export default function NavBar() {
+  // eslint-disable-next-line unicorn/no-useless-undefined
+  const [, setToken] = useLocalStorage("_n", undefined);
+  // eslint-disable-next-line unicorn/no-useless-undefined
+  const [, setUser] = useLocalStorage("_e", undefined);
+  const dispatch = useAppDispatch();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  function handleLogout() {
+    dispatch(authActions.logout());
+    const key = enqueueSnackbar("", {
+      persist: true,
+      className: "text-red-500",
+      style: { backgroundColor: "#141416", boxShadow: "none" },
+      key: 2,
+      autoHideDuration: 0,
+      transitionDuration: 0,
+    });
+    closeSnackbar(key);
+    setToken(undefined);
+    setUser(undefined);
+  }
   return (
     <div className="bg-neutral-800 *:transition relative text-xs sm:text-sm flex gap-5 items-center justify-center py-2">
-      <Link href="/chats" className="bg-sky-800 px-3 rounded-full">
+      <Link href="/chats" className="bg-sky-800 px-2 sm:px-3 rounded-full">
         Chats
       </Link>
-      <Link href="/chats" className="hover:bg-sky-800 px-3 rounded-full">
+      <Link
+        href="/chats"
+        className="hover:bg-sky-800 px-2 sm:px-3 rounded-full"
+      >
         Groups
       </Link>
-      <Link href="/chats" className="hover:bg-sky-800 px-3 rounded-full">
+      <Link
+        href="/chats"
+        className="hover:bg-sky-800 px-2 sm:px-3 rounded-full"
+      >
         People
       </Link>
+      <button
+        onClick={handleLogout}
+        className="hover:bg-sky-800 sm:px-2 rounded-full"
+      >
+        <IoMdLogOut className="text-sm sm:text-xl" />
+      </button>
     </div>
   );
 }
