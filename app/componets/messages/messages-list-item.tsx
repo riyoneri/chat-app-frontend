39e186dffa2fa8b2833a4271326339f98e75319b
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-11 19:23:16 +0200

    refactor: use reactTimeAgo on message

--- app/componets/messages/messages-list-item.tsx
+++ app/componets/messages/messages-list-item.tsx
@@ -1,10 +1,11 @@
+import { useAppSelector } from "@/app/store/hooks";
 import { MessageDto } from "@/app/util/api";
 import classNames from "classnames";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+import ReactTimeAgo from "react-time-ago";
 import MessageSVG from "./message-svg";
-
-dayjs.extend(relativeTime);
+TimeAgo.addDefaultLocale(en);
 
 export default function MessagesListItem({
   createdAt,
@@ -12,14 +13,9 @@ export default function MessagesListItem({
   content,
   displayDate,
 }: MessageDto & { displayDate?: boolean }) {
-  const now = dayjs();
-  const messageDate = dayjs(createdAt);
+  const userId = useAppSelector((state) => state.auth.user?._id);
 
-  let timeString = now.isSame(messageDate, "day")
-    ? messageDate.from(now)
-    : messageDate.format("MM/DD/YYYY");
-
-  const isMesssageIncoming = senderId !== "1";
+  const isMesssageIncoming = senderId !== userId;
 
   return (
     <div
@@ -38,13 +34,17 @@ export default function MessagesListItem({
           {content}
         </div>
         {displayDate && (
-          <p
+          <div
             className={classNames("text-xs px-1", {
               "text-end": !isMesssageIncoming,
             })}
           >
-            {timeString}
-          </p>
+            <ReactTimeAgo
+              tooltip={false}
+              date={new Date(createdAt)}
+              timeStyle="twitter"
+            />
+          </div>
         )}
       </div>
     </div>
