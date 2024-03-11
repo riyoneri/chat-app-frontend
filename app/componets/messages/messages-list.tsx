05c56fc825612f90bcce70f4b858a42ec1ed0c87
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-11 22:53:19 +0200

    refactor: scroll to bottom on message loading

--- app/componets/messages/messages-list.tsx
+++ app/componets/messages/messages-list.tsx
@@ -1,12 +1,20 @@
 import { MessageDto } from "@/app/util/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { useEffect, useRef } from "react";
 import MessagesListItem from "./messages-list-item";
 dayjs.extend(relativeTime);
 
 export default function MessagesList({ messages }: { messages: MessageDto[] }) {
+  const containerReference = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    const { current } = containerReference;
+    current && current.scrollIntoView({ behavior: "instant" });
+  }, [messages]);
+
   return (
-    <div className="flex flex-col gap-2 p-2 text-xs sm:text-sm">
+    <div className="flex flex-col gap-2 relative p-2 text-xs sm:text-sm">
       {messages.length > 0 ? (
         messages.map((message, index) => (
           <MessagesListItem
@@ -20,6 +28,9 @@ export default function MessagesList({ messages }: { messages: MessageDto[] }) {
         ))
       ) : (
         <p className="text-center my-auto italic">No messages yet</p>
+      )}
+      {messages.length > 0 && (
+        <span ref={containerReference} className="absolute bottom-0"></span>
       )}
     </div>
   );
