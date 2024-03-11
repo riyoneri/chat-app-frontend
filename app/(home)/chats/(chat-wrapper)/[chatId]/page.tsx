aacd339601044ffa984884fc9ab51a0de50d931d
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-11 16:41:37 +0200

    feat: fetch chat messages

--- app/(home)/chats/(chat-wrapper)/[chatId]/page.tsx
+++ app/(home)/chats/(chat-wrapper)/[chatId]/page.tsx
@@ -3,15 +3,42 @@
 import MessagesList from "@/app/componets/messages/messages-list";
 import { useAppDispatch } from "@/app/store/hooks";
 import { uiActions } from "@/app/store/ui-slice";
-import { Avatar, Badge } from "@material-tailwind/react";
+import { ChatWithMessagesDto } from "@/app/util/api";
+import { protectedFetch } from "@/app/util/fetchers";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Avatar, Badge, Spinner } from "@material-tailwind/react";
+import { useQuery } from "@tanstack/react-query";
+import classNames from "classnames";
 import { useParams } from "next/navigation";
-import { FormEvent } from "react";
+import { useForm } from "react-hook-form";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoIosSend } from "react-icons/io";
+import { object, string } from "yup";
+
+interface MessageFormData {
+  messageText: string;
+}
+
+const schema = object({ messageText: string().required() });
 
 export default function ChatId() {
   const parameters = useParams();
   const appDispatch = useAppDispatch();
+  const { data, error, isLoading } = useQuery<ChatWithMessagesDto>({
+    queryFn: () => protectedFetch({ url: `/chats/${parameters.chatId}` }),
+    queryKey: ["messages", parameters.chatId],
+  });
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const onSubmit = (data: MessageFormData) => {
+    console.log(data);
+    reset();
+  };
 
   return (
     <>
@@ -26,30 +53,40 @@ export default function ChatId() {
             <div className="flex-1 flex gap-2 items-center">
               <Badge color="green" overlap="circular" placement="bottom-end">
                 <Avatar
-                  src="https://docs.material-tailwind.com/img/face-2.jpg"
+                  src={
+                    data?.participants.imageUrl ??
+                    "https://i.pinimg.com/564x/09/64/03/09640380fe9e45c7cc7c786e25c09985.jpg"
+                  }
                   alt="Avatar"
                   placeholder={undefined}
                   size="sm"
                 />
               </Badge>
-              <p className="text-sm sm:text-base">Lara Muller</p>
+              <p className="text-sm sm:text-base">{data?.participants.name}</p>
             </div>
           </div>
         </div>
-        <div className="flex-1 py-1">
-          <MessagesList />
+        <div className="py-1 overflow-y-auto min-h-[calc(100dvh-9.6rem)] sm:min-h-[calc(100dvh-9.9rem)] grid place-content-center scrollbar-none">
+          {data?.messages && <MessagesList messages={data?.messages} />}
+          {error && (
+            <p className="text-sm text-neutral-300">{error?.message}</p>
+          )}
+          {isLoading && <Spinner fontSize={20} />}
         </div>
         <form
-          onSubmit={(event: FormEvent<HTMLFormElement>) =>
-            event.preventDefault()
-          }
+          onSubmit={handleSubmit(onSubmit)}
           className="flex bg-ui-darker gap-2 px-1 py-2"
         >
           <textarea
-            name="Lion"
             placeholder="Write message here..."
-            className="flex-1 resize-none min-w-1 p-1 text-sm focus:outline-none bg-neutral-700"
+            className={classNames(
+              "flex-1 resize-none min-w-1 p-1 text-sm focus:outline-none bg-neutral-700 border-2 border-neutral-700",
+              {
+                "border-red-500": errors.messageText,
+              },
+            )}
             rows={2}
+            {...register("messageText")}
           ></textarea>
           <button className="size-12 cursor-pointer grid place-content-center bg-sky-800">
             <IoIosSend className="text-2xl" />
