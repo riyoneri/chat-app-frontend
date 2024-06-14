aa068b8c0fb49f7095139238ff9eaabc2cc86dd8
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-14 09:14:53 +0200

    feat: configure app colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,15 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./components/**/*.{ts,tsx,mdx}", "./app/**/*.{ts,tsx,mdx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        secondary: "rgb(3, 94, 104)",
+        accent: "rgb(103, 136, 168)",
+        tertiary: "rgb(89, 89, 89)",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
