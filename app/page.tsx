78c480037be4d3b210f47b2ff1d4b6f32daf48f1
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-14 16:05:32 +0200

    feat: create login page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import PasswordInputLabel from "@/components/input-labels/password-input-label";
 import TextInputLabel from "@/components/input-labels/text-input-label";
 import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { object, string } from "yup";
 
@@ -22,9 +23,9 @@ export default function Home() {
   const submitHandler = (_data: object) => {};
 
   return (
-    <main className="mx-auto flex w-full flex-col justify-center gap-5 sm:w-2/3 md:w-1/2">
+    <main className="mx-auto flex w-full flex-col justify-center sm:w-2/3 md:w-1/2">
       <h3 className="text-center text-2xl font-medium">Sign in to the app</h3>
-      <form className="grid gap-3" onSubmit={handleSubmit(submitHandler)}>
+      <form className="grid gap-3 mt-5" onSubmit={handleSubmit(submitHandler)}>
         <TextInputLabel
           register={register("username")}
           name="email"
@@ -37,9 +38,23 @@ export default function Home() {
           placeHolder="Password"
           errorMessage={errors.password?.message}
         />
+        <Link
+          href="/forgot-password"
+          className="ml-auto -mt-2 hover:text-accent text-sm transition"
+        >
+          Forgot password?
+        </Link>
 
-        <button className="rounded-sm bg-secondary py-1">Sign In</button>
+        <button className="rounded-sm bg-secondary py-1 hover:bg-secondary/80 transition">
+          Sign In
+        </button>
       </form>
+
+      <span className="dui-divider text-sm">No Account?</span>
+
+      <Link href="/register" className="mx-auto hover:text-accent transition">
+        Register
+      </Link>
     </main>
   );
 }
