d8b71274613ceab608b8be04ae9d3166276d4d68
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-14 16:07:16 +0200

    feat: crete password-input-label component

---
+++ components/input-labels/password-input-label.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+
+interface PasswordInputLabelProperties {
+  name: string;
+  placeHolder: string;
+  errorMessage?: string;
+  register: UseFormRegisterReturn;
+}
+
+export default function PasswordInputLabel({
+  name,
+  placeHolder,
+  register,
+  errorMessage,
+}: PasswordInputLabelProperties) {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <label htmlFor={name} className="grid">
+      <div className="flex items-center rounded-sm bg-tertiary px-2 py-1">
+        <input
+          type={isVisible ? "text" : "password"}
+          {...register}
+          placeholder={placeHolder}
+          id={name}
+          className="flex-1 bg-transparent placeholder:text-sm focus:outline-none"
+        />
+        <span
+          className="cursor-pointer select-none text-lg"
+          onClick={() => setIsVisible(!isVisible)}
+        >
+          {isVisible ? <FaEyeSlash /> : <FaEye />}
+        </span>
+      </div>
+      {errorMessage && (
+        <span className="text-sm text-red-700">{errorMessage}</span>
+      )}
+    </label>
+  );
+}
