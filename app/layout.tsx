bd22e4d133b6f4d7acee419eda57dfe81da5eeda
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-15 20:31:49 +0200

    feat: display navigation bar on root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import NavigationBar from "@/components/navigation-bar";
 import classNames from "classnames";
 import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
 import "./globals.css";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -9,15 +13,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
   return (
     <html lang="en">
       <body
         className={classNames(
           montserrat.className,
-          "bg-neutral-900 max-w-screen-2xl mx-auto text-white min-h-dvh grid",
+          "bg-neutral-900 max-w-screen-2xl mx-auto text-white min-h-dvh flex",
         )}
       >
-        {children}
+        {!pathname.includes("auth") && pathname !== "/" && <NavigationBar />}
+        <main className="flex-1 *:h-full">{children}</main>
       </body>
     </html>
   );
