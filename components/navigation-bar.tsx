e26969a1e46f5b75f1f6b8a42e1cb080917af57f
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-15 20:30:30 +0200

    feat: create category-list-item component

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -7,13 +7,13 @@ import { TiMessages } from "react-icons/ti";
 export default function NavigationBar() {
   return (
     <nav className="relative grid place-content-center gap-3 bg-secondary/50 px-2 text-xl *:rounded-md *:bg-white/10 *:p-2 *:transition sm:*:p-3">
-      <Link href="/chat" className="p-2 hover:bg-red-500 sm:p-3">
+      <Link href="/chat" className="hover:bg-red-500">
         <TiMessages />
       </Link>
-      <Link href="/video" className="p-2 hover:bg-red-500 sm:p-3">
+      <Link href="/video" className="hover:bg-red-500">
         <MdVideoCall />
       </Link>
-      <Link href="/audio" className="p-2 hover:bg-red-500 sm:p-3">
+      <Link href="/audio" className="hover:bg-red-500">
         <IoCall />
       </Link>
 
