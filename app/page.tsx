3e6b97373d48d135bd12ec50f4a36f541b29860b
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-15 20:31:19 +0200

    feat: update homepage contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,51 @@
+import CategoryListItem from "@/components/category-list-item";
+import { IconType } from "react-icons";
+import { IoCall } from "react-icons/io5";
+import { MdVideoCall } from "react-icons/md";
+import { TiMessages } from "react-icons/ti";
+
+export interface Category {
+  id: number;
+  title: string;
+  link: string;
+  Icon: IconType;
+  className?: string;
+}
+
+const Categories: Category[] = [
+  {
+    id: 1,
+    title: "Chat",
+    link: "/chat",
+    Icon: TiMessages,
+    className: "col-span-full",
+  },
+  {
+    id: 2,
+    title: "Video Call",
+    link: "/video",
+    Icon: MdVideoCall,
+  },
+  {
+    id: 3,
+    title: "Audio Call",
+    link: "/audio",
+    Icon: IoCall,
+  },
+];
+
 export default function Home() {
-  return <main>This is homepage</main>;
+  return (
+    <>
+      <title>Categories</title>
+      <main className="grid h-full items-center px-5 xs:place-content-center xs:gap-5">
+        <h3 className="text-center text-2xl">Categories</h3>
+        <div className="grid w-full items-stretch gap-4 xs:grid-cols-2">
+          {Categories.map((category) => (
+            <CategoryListItem {...category} key={category.id} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
