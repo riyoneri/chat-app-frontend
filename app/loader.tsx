932a0ae8be880536dfa762bb9e7b40f6e8009ed2
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-17 10:59:47 +0200

    feat: remove all previous files

--- app/loader.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@material-tailwind/react";
-
-export default function Loader() {
-  return (
-    <main className="grid min-h-dvh place-content-center">
-      <Spinner className="size-10" />
-    </main>
-  );
-}
--- app/util/socket.ts
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-let token;
-if (typeof window !== "undefined") {
-  token = localStorage.getItem("_n")?.replaceAll('"', "");
-}
-
-export default io(String(process.env.NEXT_PUBLIC_API_URL), {
-  auth: { token },
-});
