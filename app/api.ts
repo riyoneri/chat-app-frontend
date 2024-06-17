92ffbb2cca30b202850f4a3668a7401ba4fff764
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-17 10:46:20 +0200

    feat: create MessageDto

--- app/api.ts
+++ app/api.ts
@@ -5,3 +5,12 @@ interface UserDto {
   name: string;
   username: string;
 }
+
+interface MessageDto {
+  id: string;
+  conversationId: string;
+  senderId: string;
+  content: string;
+  state: "pending" | "delivered" | "seen";
+  createdAt: string;
+}
