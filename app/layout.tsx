eae4b13e628565b7d35250439b79b8a963fff649
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-20 15:46:30 +0200

    chore: install @tanstack/react-query-devtools package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import NavigationBar from "@/components/navigation-bar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import classNames from "classnames";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
@@ -11,6 +13,8 @@ import "./globals.css";
 TimeAgo.addDefaultLocale(en);
 
 const montserrat = Montserrat({ subsets: ["latin"] });
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -29,8 +33,12 @@ export default function RootLayout({
           "bg-neutral-900 max-w-screen-2xl overflow-hidden mx-auto text-white min-h-dvh flex",
         )}
       >
-        {!pathname.includes("auth") && pathname !== "/" && <NavigationBar />}
-        <main className="flex-1 *:h-full">{children}</main>
+        <QueryClientProvider client={queryClient}>
+          {!pathname.includes("auth") && pathname !== "/" && <NavigationBar />}
+          <main className="flex-1 *:h-full">{children}</main>
+
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </body>
     </html>
   );
