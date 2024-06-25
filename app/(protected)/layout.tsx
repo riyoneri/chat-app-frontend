8b85dee8e0e31eb3aca46b70559af1bd7ce9f3a5
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-25 14:32:33 +0200

    feat: protect routes

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,7 +1,41 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { enqueueSnackbar } from "notistack";
+import { useEffect, useState } from "react";
+import { useLocalStorage } from "usehooks-ts";
+import useLocalstorageData from "../hooks/use-localstorage-data";
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  const data = useLocalstorageData();
+  const router = useRouter();
+  const [isMounted, setIsMounted] = useState(false);
+  const [, setTokenValue] = useLocalStorage("_o", "");
+  const [, setUserValue] = useLocalStorage("_e", "");
+
+  useEffect(() => {
+    setIsMounted(true);
+    if (!data && isMounted) {
+      setTokenValue("");
+      setUserValue("");
+      enqueueSnackbar({ message: "Login first!", variant: "error" });
+      router.replace("/auth/signin");
+    }
+  }, [data, isMounted, router, setTokenValue, setUserValue]);
+
+  return (
+    <>
+      {!isMounted || !data ? (
+        <div className="grid place-content-center">
+          <span className="dui-loading dui-loading-spinner dui-loading-lg"></span>
+        </div>
+      ) : (
+        children
+      )}
+    </>
+  );
 }
