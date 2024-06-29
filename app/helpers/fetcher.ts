c72ac7265e9120e268df62bd0cfa674958a832a8
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-29 12:29:55 +0200

    feat: handle unauthorised response in fetcher

--- app/helpers/fetcher.ts
+++ app/helpers/fetcher.ts
@@ -4,13 +4,22 @@ interface FetcherData {
   url: string;
   method?: "GET" | "POST" | "PATCH" | "OPTIONS" | "DELETE";
   body?: FormData | string;
+  logout?: () => void;
 }
 
-export const fetcher = async ({ url, body, method = "GET" }: FetcherData) => {
+export const fetcher = async ({
+  url,
+  body,
+  method = "GET",
+  logout,
+}: FetcherData) => {
   try {
     let headers: any = {};
+    const token = localStorage.getItem("_o")?.replaceAll(/"/g, "");
 
     typeof body === "string" && (headers["Content-Type"] = "application/json");
+
+    token && (headers["Authorization"] = `Bearer ${token}`);
 
     const response = await fetch(`${API_URL}${url}`, {
       method,
@@ -19,6 +28,7 @@ export const fetcher = async ({ url, body, method = "GET" }: FetcherData) => {
     });
 
     const data = await response.json();
+
     if (!response.ok) {
       if (typeof data.message === "string")
         throw { errorMessage: data.message, status: response.status };
@@ -31,7 +41,10 @@ export const fetcher = async ({ url, body, method = "GET" }: FetcherData) => {
 
     return data;
   } catch (error) {
-    const typedError = error as Error;
+    const typedError = error as Error & { status: number };
+
+    if (typedError.status === 401 && logout) logout();
+
     throw {
       errorMessage:
         typeof typedError?.message === "string"
