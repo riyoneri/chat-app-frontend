a7220e6525e23f557fdd8b2a4d0b9183bb909adf
Lionel Kaneza <lionkaneza@gmail.com>
2024-07-06 09:44:56 +0200

    feat: emit user:logout socket event on logout

--- app/hooks/use-logout.ts
+++ app/hooks/use-logout.ts
@@ -1,18 +1,22 @@
 import { useRouter } from "next/navigation";
 import { useLocalStorage } from "usehooks-ts";
+import { getSocket } from "../helpers/socket";
 import { authActions } from "../store/slices/auth.slice";
-import { useAppDispatch } from "./store-hooks";
+import { useAppDispatch, useAppSelector } from "./store-hooks";
 
 export default function useLogout() {
   const removeTokenValue = useLocalStorage("_o", "_")[2];
   const removeUserValue = useLocalStorage("_e", "_")[2];
+  const userId = useAppSelector((state) => state.auth.id);
   const dispatch = useAppDispatch();
   const router = useRouter();
+  const socket = getSocket();
 
   return () => {
     dispatch(authActions.signout());
     removeTokenValue();
     removeUserValue();
     router.replace("/auth/signin");
+    socket.emit("user:logout", userId);
   };
 }
