641e2b244894f0741122be2213ac7a474c3a5874
Lionel Kaneza <lionkaneza@gmail.com>
2024-07-10 17:10:53 +0200

    feat: create use-chat-id hook

---
+++ app/hooks/use-chat-id.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetcher } from "../helpers/fetcher";
+import useLogout from "./use-logout";
+
+interface ResponseError {
+  errorMessage?: string;
+  message: {
+    chatId: { message: string };
+  };
+}
+
+export const useChatId = (id: string) => {
+  const logout = useLogout();
+  const {
+    data: chatData,
+    error: chatError,
+    isLoading: chatIsLoading,
+  } = useQuery<
+    object,
+    ResponseError,
+    { chat: ChatDto; messages: MessageDto[] }
+  >({
+    queryFn: () => fetcher({ url: `/chats/${id}`, logout }),
+    queryKey: ["chat", id, logout],
+  });
+
+  return { chatData, chatError, chatIsLoading };
+};
