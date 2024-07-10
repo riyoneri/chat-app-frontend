7973d05b0cf35dff188ef50ce69085b3921c795b
Lionel Kaneza <lionkaneza@gmail.com>
2024-07-10 17:12:16 +0200

    feat: create message-list component

---
+++ components/messages/message-list.tsx
@@ -0,0 +1,15 @@
+import MessagesListItem from "../messages-list-item";
+
+export default function MessageList({ messages }: { messages: MessageDto[] }) {
+  return (
+    <div>
+      {messages.length > 0 ? (
+        messages.map((message) => (
+          <MessagesListItem key={message.id} {...message} />
+        ))
+      ) : (
+        <p className="text-center italic">No messages</p>
+      )}
+    </div>
+  );
+}
