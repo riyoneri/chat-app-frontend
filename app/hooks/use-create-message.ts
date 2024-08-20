bcbb9af844d675e5bb9e5f98443c2783eededd90
Lionel Kaneza <lionkaneza@gmail.com>
2024-08-20 10:56:45 +0200

    feat: create custom hook for fetching the creation of message

---
+++ app/hooks/use-create-message.ts
@@ -0,0 +1,38 @@
+import { useMutation } from "@tanstack/react-query";
+import { fetcher } from "../helpers/fetcher";
+
+interface ResponseProperty {
+  location: "string";
+  message: string;
+}
+
+interface MessageFormData {
+  text: ResponseProperty;
+  image: ResponseProperty;
+  video: ResponseProperty;
+  voice_note: ResponseProperty;
+}
+
+interface ResponseError {
+  errorMessage?: string;
+  message: Partial<MessageFormData>;
+}
+
+export default function useCreateMessage() {
+  const {
+    mutate,
+    data: createMessageData,
+    error: createMessageError,
+    isPending: createMessageIsPending,
+  } = useMutation<object, ResponseError, FormData>({
+    mutationFn: (data: FormData) =>
+      fetcher({ url: "/chats/message", method: "POST", body: data }),
+  });
+
+  return {
+    mutate,
+    createMessageData,
+    createMessageError,
+    createMessageIsPending,
+  };
+}
